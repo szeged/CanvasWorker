2a0236c31addac6391eafa8fcfe7dbfe8cbba54d
Gabor Loki <loki@inf.u-szeged.hu>
2015-09-18 11:49:25 +0200

    Setup initial build env.

--- build.sh
+++ build.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-RELDIR=`basename $0;`
-BASE=`cd $0;pwd`
+RELDIR=`dirname $0;`
+BASE=`cd $RELDIR;pwd`
+
 
 cd $BASE
 
 LKGR=`cat LKGR`
+LKGR_WK=`cat LKGR-WebKit`
 
 if [ ! -d src ]
 then
+  mkdir tmp
+  cd tmp
+
+  # Download Chromium source
   fetch --nohooks chromium
+
+  # Install dependencies
   cd src
   ./build/install-build-deps.sh
   gclient runhooks
 
+  # Move everything to its final place
+  cd $BASE/tmp
+  mv $BASE/tmp/src $BASE/
+
+  # Move to Last Known Good Revision
   git checkout -B canvasWorker $LKGR
-  cd ..
+  gclient sync
+  cd third_party/WebKit
+  git checkout -B canvasWorker $LKGR_WK
+
+  # Setup build env
+  cd $BASE/src
+  gn gen out/Release
+  gn args out/Release
 fi
 
-cd src
+cd $BASE/src
 
-gn gen out/Release
-gn args out/Release
+# Start the build
 ninja -C out/Release chrome
-
-cd ..
